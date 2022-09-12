b64c7de2a9769668e19278e141d38d046a40e1db
MaiGoher <91436067+MaiGoher@users.noreply.github.com>
2022-09-13 01:50:30 +0200

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print alphapet 
+ * Return: 0
+ */
+
+int main(void)
+{
+int count = 'a';
+while (count <='z' || count >='a')
+{
+putchar(count);
+count += 1;
+}
+putchar('\n');
+return (0);
+}
