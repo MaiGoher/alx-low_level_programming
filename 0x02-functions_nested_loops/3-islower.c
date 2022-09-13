6420ea2205aa17c453409f9e8d384bebc2a5b0c9
MaiGoher <91436067+MaiGoher@users.noreply.github.com>
2022-09-14 00:05:20 +0200

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _islower(int c) - return 1 is lowercase
+ * retrun 0 otherwise
+ * Return: nothing.
+ */
+
+int _islower(int c)
+{
+if(c <= 'z' || c >= 'a')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
