c4358d0b8d73b0eaa6bcc997c0779bc442d8741f
MaiGoher <91436067+MaiGoher@users.noreply.github.com>
2022-09-13 02:03:24 +0200

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,8 @@ int main(void)
 int count = 'a';
 while (count <= 'z' &&  count >= 'a')
 {
-if(count != 'q' && count != 'e') putchar(count);
+if (count != 'q' && count != 'e') 
+putchar(count);
 count += 1;
 }
 putchar('\n');
