1dc1a703564e1e040b7fcf5b6409c8bc5a787a02
MaiGoher <91436067+MaiGoher@users.noreply.github.com>
2022-09-20 04:05:06 +0200

    3-puts.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,17 +11,14 @@ void print_rev(char *s)
 {
 	int i;
 
-	i = 0;
-
-	while (*(s + i) != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
-	i = i - 1;
-	while (i >= 0)
+	i--;
+	while (s[i] != '\0')
 	{
-		_putchar(*(s + i));
-		i--;
+		_putchar(s[i--]);
 	}
 	_putchar('\n');
 }
