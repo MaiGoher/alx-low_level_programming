11d26f702662ea6fe4b774ee2c1901925a7d05d6
MaiGoher <91436067+MaiGoher@users.noreply.github.com>
2022-09-20 03:42:44 +0200

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,12 +4,18 @@
 /**
  * int _strlen - a function that returns the length of a string
  * @s: input
- * Return: str
+ * Return: sum
  */
  int _strlen(char *s)
 {
-int str;
-str = sizeof(*s);
-return str;
+int i = 1, sum = 0;
+	char pl = s[0];
+
+	while (pl != '\0')
+	{
+		sum++;
+		pl = s[i++];
+	}
+	return (sum);
 	
 }
