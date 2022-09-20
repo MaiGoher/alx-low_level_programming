1b92189c318afd0e9cd16cf4001327f5d4306be8
MaiGoher <91436067+MaiGoher@users.noreply.github.com>
2022-09-20 04:27:14 +0200

    7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * puts_half - prints the last half of a string.
  * @str: tested string
@@ -29,7 +30,7 @@ void puts_half(char *str)
 
 	while (*(str + j) != '\0')
 	{
-		putchar(*(str + j));
+		printf(" %c " , *(str + j));
 		j++;
 	}
 	putchar('\n');
