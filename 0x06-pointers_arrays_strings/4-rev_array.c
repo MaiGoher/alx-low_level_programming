1f8ff0cdecf641ad70864efa2476ba83512709c9
MaiGoher <91436067+MaiGoher@users.noreply.github.com>
2022-09-23 01:58:21 +0200

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,16 +7,15 @@
  */
 void reverse_array(int *a, int n)
 {
-    int swap, begin, end;
-
-	begin = 0;
-	end = n - 1;
-	while (begin < end)
-	{
-		swap = *(a + begin);
-		*(a + begin) = *(a + end);
-		*(a + end) = swap;
-		begin++;
-		end--;
-	}
+int swap, begin, end;
+begin = 0;
+end = n - 1;
+while (begin < end)
+{
+swap = *(a + begin);
+*(a + begin) = *(a + end);
+*(a + end) = swap;
+begin++;
+end--;
+}
 }
