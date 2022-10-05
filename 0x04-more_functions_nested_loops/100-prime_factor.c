6350a5b1cfca2345503cc213410b89232b2e8d50
MaiGoher <91436067+MaiGoher@users.noreply.github.com>
2022-10-06 00:10:46 +0200

    readme file

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * main - prints the biggest prime factor of a number.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int number;
+
+	number = 612852475143;
+
+	if (isPrime(number) == 1)
+	{
+		printf("%ld\n", number);
+	}
+	else
+	{
+		printf("%ld\n", biggestFactor(number));
+	}
+	return (0);
+}
 
 /**
  * isPrime - analise if a number is prime or not
